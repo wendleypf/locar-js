1948fe8e67aab2723bcd6f660f0ac7a851b60388
Wendley Paulo de França <wendley.franca@virtus.ufcg.edu.br>
2026-07-05 00:08:02 -0300

    location box

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import dynamic from 'next/dynamic'
 
 const DynamicAr = dynamic(() => import('@/components/ar'), {
+    ssr: false,
     loading: () => <p>carregando...</p>
 })
 
--- components/ar/index.tsx
+++ components/ar/index.tsx
@@ -7,6 +7,8 @@ import * as THREE from 'three';
 export default function Ar() {
     const arRef = React.useRef(null);
     const canvaRef = React.useRef<HTMLCanvasElement>(null);
+    const [firstLocation, setFirstLocation] = React.useState(true);
+
     React.useEffect(() => {
         const initLocar = async () => {
             if (!canvaRef.current) {
@@ -14,27 +16,54 @@ export default function Ar() {
             }
             try {
                 const locar = await locAr(canvaRef.current);
-                locar.on("gpserror", (error : GeolocationPositionError) => {
+                locar.on('gpserror', (error: GeolocationPositionError): void => {
                     console.log(`GPS error: ${error.code}`);
                 });
-                locar.on("gpsupdate", (ev: GpsReceivedEvent) => {
-                    console.log("GPS update", ev);
-                    const geom = new THREE.BoxGeometry(10,10,10);
-                    const mesh = new THREE.Mesh(geom, new THREE.MeshBasicMaterial({color: 0xff0000 }));
-                    locar.add(mesh, ev.position.coords.longitude, ev.position.coords.latitude + 0.0005);
-                })
-                // const geom = new THREE.BoxGeometry(10, 10, 10);
-                // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-                // const mesh = new THREE.Mesh(geom, material);
-                // locar.add(mesh, -0.72, 51.0505);
+                locar.on('gpsupdate', (event: GpsReceivedEvent): void => {
+                    if (firstLocation) {
+                        const boxProps = [{
+                            latDis: 0.0005,
+                            lonDis: 0,
+                            colour: 0xff0000
+                        }, {
+                            latDis: -0.0005,
+                            lonDis: 0,
+                            colour: 0xffff00
+                        }, {
+                            latDis: 0,
+                            lonDis: -0.0005,
+                            colour: 0x00ffff
+                        }, {
+                            latDis: 0,
+                            lonDis: 0.0005,
+                            colour: 0x00ff00
+                        }];
+                        const geom = new THREE.BoxGeometry(10,10,10);
+
+                        for(const boxProp of boxProps) {
+                            const mesh = new THREE.Mesh(
+                                geom,
+                                new THREE.MeshBasicMaterial({color: boxProp.colour})
+                            );
+
+                            locar.add(
+                                mesh,
+                                event.position.coords.longitude + boxProp.lonDis,
+                                event.position.coords.latitude + boxProp.latDis
+                            );
+                        }
+                    }
+                    setFirstLocation((old: boolean): boolean => !old)
+                });
                 await locar.startGps();
             } catch (e: unknown) {
                 console.log(e);
             }
         }
         initLocar().then();
-    }, [])
-    return <div ref={arRef} className='w-screen h-screen'>
+    }, [firstLocation]);
+
+    return (<div ref={arRef} className='w-screen h-screen'>
         <canvas ref={canvaRef}></canvas>
-    </div>
+    </div>)
 }
